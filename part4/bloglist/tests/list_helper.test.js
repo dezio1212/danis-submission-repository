ec3d1712c25a356a15ca501807cdbbd0ade1633d
Danis <danisezio1212@gmail.com>
2025-11-02 13:04:36 +0700

    Part 4: part4.5 - favoriteBlog helper + tests

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,98 @@
-const { test } = require('node:test')
+// tests/list_helper.test.js
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+// --- 4.3: dummy ---
 test('dummy returns one', () => {
-    const result = listHelper.dummy([])
-    assert.strictEqual(result, 1)
+  const result = listHelper.dummy([])
+  assert.strictEqual(result, 1)
+})
+
+// --- Data contoh global untuk 4.4+ ---
+const listEmpty = []
+
+const listWithOneBlog = [
+  {
+    id: 'x1',
+    title: 'Single',
+    author: 'Author A',
+    url: 'http://example.com/1',
+    likes: 5
+  }
+]
+
+const listWithManyBlogs = [
+  {
+    id: 'a1',
+    title: 'React tips',
+    author: 'Dev One',
+    url: 'http://example.com/a1',
+    likes: 7
+  },
+  {
+    id: 'a2',
+    title: 'Node patterns',
+    author: 'Dev Two',
+    url: 'http://example.com/a2',
+    likes: 5
+  },
+  {
+    id: 'a3',
+    title: 'Testing 101',
+    author: 'Dev Three',
+    url: 'http://example.com/a3',
+    likes: 12
+  },
+  {
+    id: 'a4',
+    title: 'Express middlewares',
+    author: 'Dev Two',
+    url: 'http://example.com/a4',
+    likes: 10
+  }
+] // total likes = 34, favorit = a3 (12)
+
+// --- 4.4: totalLikes ---
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(listEmpty)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 34)
+  })
+})
+
+// --- 4.5: favoriteBlog ---
+describe('favorite blog', () => {
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog(listEmpty)
+    assert.strictEqual(result, null)
+  })
+
+  test('when list has only one blog equals that blog (title, author, likes)', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      title: 'Single',
+      author: 'Author A',
+      likes: 5
+    })
+  })
+
+  test('of a bigger list is the one with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    assert.deepStrictEqual(result, {
+      title: 'Testing 101',
+      author: 'Dev Three',
+      likes: 12
+    })
+  })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,22 @@
 const dummy = (_blogs) => 1
 
-module.exports = { dummy }
+const totalLikes = (blogs = []) => {
+  return blogs.reduce((sum, b) => sum + (b.likes || 0), 0)
+}
+
+const favoriteBlog = (blogs = []) => {
+  if (!blogs.length) return null
+  const top = blogs.reduce((max, b) => {
+    const maxLikes = max?.likes ?? 0
+    const bLikes = b?.likes ?? 0
+    return bLikes > maxLikes ? b : max
+  }, blogs[0])
+
+  return {
+    title: top.title,
+    author: top.author,
+    likes: top.likes ?? 0
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
