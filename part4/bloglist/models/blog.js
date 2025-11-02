1d7b11bebbd32ee6927093945266b8cb5e6ea95f
Danis <danisezio1212@gmail.com>
2025-11-02 12:08:21 +0700

    Part 4: part4.1 - Blog List step 1 (Express+Mongoose, GET/POST /api/blogs)

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const blogSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    author: { type: String, required: true, trim: true },
+    url: { type: String, required: true, trim: true },
+    likes: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
+
+blogSchema.set('toJSON', {
+  transform: (_doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+module.exports = model('Blog', blogSchema);
