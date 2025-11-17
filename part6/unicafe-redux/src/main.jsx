94220fb03e095d2e1d2abe9b02a8d8924013cb8c
Danis <danisezio1212@gmail.com>
2025-11-17 13:02:40 +0700

    Part 6: part6.2 - Unicafe revisited (UI + dispatch + render loop)

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
-import counterReducer from './reducer.js'
+import counterReducer from './reducer'
 
 const store = createStore(counterReducer)
 
 const App = () => {
-  const state = store.getState()
+  const { good, ok, bad } = store.getState()
 
-  const good = () => store.dispatch({ type: 'GOOD' })
-  const ok   = () => store.dispatch({ type: 'OK' })
-  const bad  = () => store.dispatch({ type: 'BAD' })
-  const zero = () => store.dispatch({ type: 'ZERO' })
+  const goodClick = () => store.dispatch({ type: 'GOOD' })
+  const okClick   = () => store.dispatch({ type: 'OK' })
+  const badClick  = () => store.dispatch({ type: 'BAD' })
+  const zeroClick = () => store.dispatch({ type: 'ZERO' })
 
   return (
     <div style={{ fontFamily: 'sans-serif', maxWidth: 480, margin: '24px auto' }}>
       <h2>Unicafe (Redux)</h2>
 
       <div style={{ marginBottom: 12 }}>
-        good {state.good} | ok {state.ok} | bad {state.bad}
+        good {good} | ok {ok} | bad {bad}
       </div>
 
       <div>
-        <button onClick={good}>good</button>
-        <button onClick={ok} style={{ marginLeft: 8 }}>ok</button>
-        <button onClick={bad} style={{ marginLeft: 8 }}>bad</button>
-        <button onClick={zero} style={{ marginLeft: 8 }}>reset</button>
+        <button onClick={goodClick}>good</button>
+        <button onClick={okClick} style={{ marginLeft: 8 }}>ok</button>
+        <button onClick={badClick} style={{ marginLeft: 8 }}>bad</button>
+        <button onClick={zeroClick} style={{ marginLeft: 8 }}>reset</button>
       </div>
     </div>
   )
